3cf101e0aa24391f95c30df852e349b686c71c73
Ítalo H. <italodssouza.backup@gmail.com>
2025-08-26 17:13:24 -0300

    First commit

---
+++ src/main/java/tech/kvothe/leaderboard/LeaderboardApplication.java
@@ -0,0 +1,13 @@
+package tech.kvothe.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardApplication.class, args);
+	}
+
+}
